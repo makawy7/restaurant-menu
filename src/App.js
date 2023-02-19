c5b3f2715e53ee355da1f1674931d587e693fe1e
abdallah <abdallahelmekawy6@gmail.com>
2023-02-19 23:14:57 +0200

    implementing functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import Item from "./Item";
+import menu from "./data";
+import { useState } from "react";
+
 function App() {
+  const [items, setItems] = useState(menu);
+  const setCategory = (catName) => {
+    if (catName === "all") {
+      setItems(menu);
+    } else {
+      setItems(menu.filter((item) => item.category === catName));
+    }
+  };
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="btn-container">
+          <button className="filter-btn" onClick={() => setCategory("all")}>
+            all
+          </button>
+          <button
+            className="filter-btn"
+            onClick={() => setCategory("breakfast")}
+          >
+            breakfast
+          </button>
+          <button className="filter-btn" onClick={() => setCategory("lunch")}>
+            lunch
+          </button>
+          <button className="filter-btn" onClick={() => setCategory("shakes")}>
+            shakes
+          </button>
+        </div>
+        <div className="section-center">
+          {items.map((item) => (
+            <Item key={item.id} {...item} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Item.js
@@ -0,0 +1,16 @@
+function Item({ id, title, category, price, img, desc }) {
+  return (
+    <article className="menu-item">
+      <img src={img} alt={title} className="photo" />
+      <div className="item-info">
+        <header>
+          <h4>{title}</h4>
+          <h4 className="price">${price}</h4>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </article>
+  );
+}
+
+export default Item;
